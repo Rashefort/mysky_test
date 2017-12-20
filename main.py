e4936204725c0442bf5b465905a67a5da9c0de4f
andrewisakov <andrew.isakov@gmail.com>
2017-12-20 07:06:49 +0300

    чистка main.py

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ from routes import routes_setup
 import settings
 
 
-clients = []
-
-
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     tornado.options.define('log_file_max_size', default=str(10*1024*1024))
-    tornado.options.define('log_file_prefix', default='tmtapi_server.log')
+    tornado.options.define('log_file_prefix', default='mysky_test.log')
     app = tornado.web.Application(routes_setup(), **settings.settings)
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(port=settings.SERVICE['port'],
